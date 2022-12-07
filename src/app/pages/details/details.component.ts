73f17ba62f04f6d7438c0529132ffef7bdf48011
DiegoFaustino <fsantos.diego11@gmail.com>
2022-12-07 07:37:03 -0300

    Comite inicial

---
+++ src/app/pages/details/details.component.ts
@@ -0,0 +1,43 @@
+import { PokeapiService } from './../../service/pokeapi.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit {
+
+  private urlPokemon:string = 'https://pokeapi.co/api/v2/pokemon'
+  private urlName:string = 'https://pokeapi.co/api/v2/pokemon-species'
+
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
+  constructor(
+    private activateRoute:ActivatedRoute,
+    private pokeapiService:PokeapiService
+  ) { }
+
+  ngOnInit(): void {
+    this.getPokemon
+  }
+
+  public getPokemon(){
+    const id = this.activateRoute.snapshot.params['id'];
+    const pokemon = this.pokeapiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeapiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon,name]).subscribe(
+      res => {
+        this.pokemon = res;
+        this.isLoading = true;
+      }
+    )
+
+  }
+
+}
